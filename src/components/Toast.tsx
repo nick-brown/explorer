ccb515acff394ea0d45c6abe6d04b9f10c57e7ab
Nick Brown <nick@nickbrown.io>
2020-05-06 02:43:00 -0700

    Style toast; rename context setter

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { useCurrentToast } from "../providers/toast";
+import { useToast } from "../providers/toast";
 
 export default function Toast() {
-  const { toastMessage, setToastMessage } = useCurrentToast();
+  const { toastMessage, setToastMessage } = useToast();
   if (toastMessage === "") return null;
 
   setTimeout(() => setToastMessage(""), 2000);
   return toastMessage === "" ? null : (
-    <div style={{ background: "#fcc" }}>{toastMessage}</div>
+    <div className={`toast fade ${toastMessage === "" ? "hide" : "show"}`} role="alert">
+      <div className="toast-header">{toastMessage}</div>
+    </div>
   );
 }
--- src/providers/toast.tsx
+++ src/providers/toast.tsx
@@ -28,10 +28,10 @@ export function ToastProvider({ children }: ToastProviderProps) {
   );
 }
 
-export function useCurrentToast() {
+export function useToast() {
   const context = useContext(ToastContext);
   if (!context) {
-    throw new Error(`useCurrentToast must be used within a ToastProvider`);
+    throw new Error(`useToast must be used within a ToastProvider`);
   }
   return context;
 }
